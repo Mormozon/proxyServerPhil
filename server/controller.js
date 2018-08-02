245e73250ccae83f8436f7e8fd057e3d4c6e08ac
Philip So <philtheso@gmail.com>
2018-08-02 16:18:46 -0700

    Server listening

---
+++ server/controller.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+
+const controller = {
+  getQuestion: (req, res) => {
+
+  },
+  getAnswer: (req, res) => {
+
+  },
+  getProduct: (req, res) => {
+
+  },
+  getRelated: (req, res) => {
+
+  },
+  loadCustomerQuestions: (req, res) => {
+
+  },
+  loadRelatedProducts: (req, res) => {
+
+  },
+  loadProductDescription: (req, res) => {
+
+  }
+}
+
+module.exports = controller;
